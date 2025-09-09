c88915e62b5deb517498b50907ce63ab2532d717
Aryan-verma11 <av868400@gmail.com>
2025-09-09 15:58:49 +0530

    Initial commit

---
+++ CHAPTER1/cmnt.py
@@ -0,0 +1,36 @@
+
+#hashtag (#) 
+# it is used for single line cmnt 
+
+
+
+
+"""it is used for 
+multiple line cmnt on python programmimg
+language"""
+
+
+
+
+'''after some time i also know this that single comma are also
+used in putting the multi line cmnt in 
+python program'''
+#this the print fun to print any thing
+#here i print the twinkle twinkle little star poem 
+
+print('''"Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky."
+"Twinkle, twinkle, little star,
+How I wonder what you are!
+When the blazing sun is gone,
+When he nothing shines upon." 
+"Twinkle, twinkle, little star,
+How I wonder what you are!
+Then you show your little light,
+Twinkle, twinkle, all the night''')
+
+# tip:-
+'''we can also use these commas in print function to print multiple lines '''
+print('''like this''')
---
+++ CHAPTER1/module.py
@@ -0,0 +1,13 @@
+#we can import libraries bys using import
+import pyjokes
+
+joke=pyjokes.get_joke()
+
+print(joke)
+
+
+#tip:-
+# you can use python by accesing terminal or command prompt 
+# by simply typing python first
+# second give calculator operation after>>>
+# this is happen by REPL stands for:-(read evaluate print loop)
