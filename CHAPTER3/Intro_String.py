0a114ab38f64d1da407bce2a026c493c72067961
Aryan-verma11 <av868400@gmail.com>
2025-09-11 11:26:00 +0530

    Add files via upload

---
+++ CHAPTER3/Intro_String.py
@@ -0,0 +1,7 @@
+#in the name variable i put my name as a string
+name="aryan"
+ 
+index=name[0:4] #0=a,1=r,2=y,3=a,4=n here the last 4th index does not print 
+print(index) 
+character1=name[2] #it will print the string with index 1 which is 1=r 
+print(character1)
---
+++ CHAPTER3/negative_slicing.py
@@ -0,0 +1,12 @@
+name="kamalhasan"
+#0=k,1=a,2=m,3=a,4=l,5=h,6=a,7=s,8=a,9=n
+print(name[0:3])
+print(name[-5:-1]) #negative slicing
+print(name[1:6])
+print(name[5:])
+print(name[:6])
+
+
+
+alpha="abcdefghijklmnopqestuvwxyz"
+print(alpha[4:15:3])  #4=e the index number start with the index 15 tells ki kaha tak jana hai 3 will define the gaping b/w them 
