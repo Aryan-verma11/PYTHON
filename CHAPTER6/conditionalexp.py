4ffed88aaf666c2028a60a21cc3a6b22271e6dfc
Aryan-verma11 <av868400@gmail.com>
2025-09-14 12:23:44 +0530

    Add files via upload

---
+++ CHAPTER6/conditionalexp.py
@@ -0,0 +1,19 @@
+# USE OF
+# if
+# else
+# elif
+
+a=int(input("enter your age: "))
+if(a>18):
+    print("you are above the age")
+elif(a<0):
+    print("you are entering the wrong age")
+elif(a==18):
+    print("you are exactly 18 years old")
+else:
+    print("you are below the age of consent")
+
+
+print("end of programm")
+
+# known as if il if else ladder
